2f26119a37b6faadba6908240b6f508fd616fb5a
Preston Elliott <preston.elliott24@gmail.com>
2019-04-12 19:19:59 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,3 +1,17 @@
-def word_substituter
+dictionary = {
+  "hello" => "hi",
+  "to, two, too" => "2",
+  "be" => "b",
+  "you" => "u",
+  "at" => "@",
+  "and" => "&"
+}
 
-end
+
+
+
+
+# def word_substituter(string)
+#
+#
+# end
