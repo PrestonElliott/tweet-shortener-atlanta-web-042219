e78f29b4d29f363f1bcec81355a0e37d41e3ec4c
Preston Elliott <preston.elliott24@gmail.com>
2019-04-12 19:46:22 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -31,7 +31,7 @@ end
 
 def selective_tweet_shortener(tweet)
   if tweet.length > 140
-    puts word_substituter(tweet)
+    puts bulk_tweet_shortener(tweet)
   elsif tweet.length < 140
     tweet
   end
